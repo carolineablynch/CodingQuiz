98d1c9555a4fc696d71bf599e9fb2c2d08ec4060
Caroline Lynch <carolineablynch@gmail.com>
2021-04-07 22:17:56 -0400

    declaring consts

--- src/Question.js
+++ src/Question.js
@@ -5,3 +5,15 @@ import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { Form, Button } from "react-bootstrap";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
+const renderers = {
+    code: ({ value }) => <SyntaxHighlighter style={a11yDark} language="javascript" children={value} />
+  }
+  const Hr = () => <hr style={{ borderTop: "1px solid rgba(0, 0, 0, 0.3)" }} />;
+  const prevNextStyle = { marginLeft: 5, marginRight: 5 };
+  const backgroundColorForChoice = ({ answer, correct }, index, finished) => {
+    if(!finished) return "white";
+    if(answer !== correct && answer === index) return "rgba(255, 0, 0, 0.3)";
+    if(answer === index || correct === index) return "rgba(0, 128, 0, 0.3)";
+  }
+
+  

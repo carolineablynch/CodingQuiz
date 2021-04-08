777fcf27c4a3a045e1d5ad8a120dc6eb6240be0d
Caroline Lynch <carolineablynch@gmail.com>
2021-04-07 22:25:07 -0400

    creating results.js and importing

---
+++ src/Results.js
@@ -0,0 +1,4 @@
+import { useState } from "react";
+import { Jumbotron, Modal, Button, InputGroup, FormControl, Row, Col } from "react-bootstrap";
+import Loader from "react-loader-spinner";
+import Scores from "./Scores";

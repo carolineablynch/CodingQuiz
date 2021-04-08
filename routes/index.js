4532f665b1913f3a5cd462e2389e9e75f8bce5d8
Caroline Lynch <carolineablynch@gmail.com>
2021-04-07 22:10:25 -0400

    index.js routes

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,21 @@
 const router = require("express").Router();
 const { Score } = require("../models");
+
+router.post("/api/scores", async (req, res) => {
+    try {
+      const { score, initials } = req.body;
+      const newScore = new Score({ score, initials });
+  
+      await newScore.save();
+      
+      const scores = await Score.find();
+  
+      res.json(scores);
+    } catch(error) {
+      res.sendStatus(500);
+  
+      throw error;
+    }
+  });
+  
+  module.exports = router;

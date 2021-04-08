7750bb297d5549285b68b9d9a931d898300f75d0
Caroline Lynch <carolineablynch@gmail.com>
2021-04-07 22:21:05 -0400

    finishing question numbers

--- src/QuestionNumbers.js
+++ src/QuestionNumbers.js
@@ -13,3 +13,23 @@ const variant = ({ answer, correct }, finished) => {
   
   return "primary";
 }
+
+export default function QuestionNumbers({ questions, finished, onChange }) {
+    return (
+      <ButtonGroup aria-label="Question Numbers">
+        {questions.map((question, index) => (
+          <Button
+            key={String(index)}
+            variant={variant(question, finished)}
+            style={{ marginLeft: 5, marginRight: 5 }}
+            onClick={() => onChange(index)}
+          >
+            {(question.answer !== undefined || finished) && buttonLogo(question)}
+            {" "}
+            {index + 1}
+          </Button>
+        ))}
+      </ButtonGroup>
+    );
+  }
+  

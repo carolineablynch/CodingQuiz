93642f726b733ca77208245d5f744d293ee93a2d
Caroline Lynch <carolineablynch@gmail.com>
2021-04-07 22:39:32 -0400

    finishing server

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ const routes = require("./routes/index");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/score", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.use(express.static("build", { extensions: ["html"] }));
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(routes);
+
+app.listen(PORT, () => console.log(`🚀 Listening on PORT ${PORT}`));

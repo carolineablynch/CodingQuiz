aa888d6fd83bbc3b66326f3270f0ae8ccb2ed1df
Caroline Lynch <carolineablynch@gmail.com>
2021-04-07 22:15:50 -0400

    building out App function for app.js file

--- src/App.js
+++ src/App.js
@@ -3,3 +3,22 @@ import { Button } from "react-bootstrap";
 import { FaRegClock } from "react-icons/fa";
 import Quiz from "./Quiz";
 import "./App.css";
+
+function App() {
+    const [started, setStarted] = useState(false);
+  
+    return (
+      <div className="App">
+        <h2>Coding Quiz</h2>
+        {started ? <Quiz /> :
+          <Button variant="primary" onClick={() => setStarted(true)}>
+            <p>Start Quiz</p>
+            3 Minutes
+            {" "}
+            <FaRegClock size={20} color="white" />
+          </Button>}
+      </div>
+    );
+  }
+  
+  export default App;

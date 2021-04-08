a19a0e075a80de732232f82e33b5ba62d8ae0f83
Caroline Lynch <carolineablynch@gmail.com>
2021-04-07 22:17:28 -0400

    creating question.js file and finished building app.test.js

---
+++ src/Question.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { Form, Button } from "react-bootstrap";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+

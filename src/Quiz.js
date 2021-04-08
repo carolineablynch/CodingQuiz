c47e52e51787f9618876dd39e1891206d00fad22
Caroline Lynch <carolineablynch@gmail.com>
2021-04-07 22:22:17 -0400

    creating Quiz.js file

---
+++ src/Quiz.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+import { Carousel, Row, Col, Button } from "react-bootstrap";
+import Question from "./Question";
+import QuestionNumbers from "./QuestionNumbers";
+import Results from "./Results";
+import Timer from "./Timer";

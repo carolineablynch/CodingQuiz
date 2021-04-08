e9f5faceb26f6bc0eb2f25e891c6aef00146fbd0
Caroline Lynch <carolineablynch@gmail.com>
2021-04-07 22:25:42 -0400

    results function

--- src/Results.js
+++ src/Results.js
@@ -2,3 +2,14 @@ import { useState } from "react";
 import { Jumbotron, Modal, Button, InputGroup, FormControl, Row, Col } from "react-bootstrap";
 import Loader from "react-loader-spinner";
 import Scores from "./Scores";
+
+export default function Results({ questions }) {
+    const correct = questions.filter(question => question.answer === question.correct).length;
+    const score = (correct / questions.length) * 100;
+    const [initials, setInitials] = useState(null);
+    const [fetching, setFetching] = useState(false);
+    const [error, setError] = useState(false);
+    const [scores, setScores] = useState(null);
+    const [show, setShow] = useState(true);
+    const handleClose = () => setShow(false);
+  
